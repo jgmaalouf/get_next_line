1280374a6f6042d0e6cfc73d2a71da9141c660c7
Jad Maalouf <jad2maalouf@gmail.com>
2022-05-23 19:43:36 +0200

    done

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,164 +6,56 @@
 /*   By: jmaalouf <jmaalouf@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 16:22:44 by jmaalouf          #+#    #+#             */
-/*   Updated: 2022/05/17 17:32:59 by jmaalouf         ###   ########.fr       */
+/*   Updated: 2022/05/21 14:45:44 by jmaalouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-t_buffer	*buffer_setup()
+char	buffer_read_char(t_buffer *buf)
 {
-	t_buffer	*ptr;
+	char	c;
 
-	ptr = (t_buffer *)malloc(sizeof(t_buffer));
-	ptr->index_read = 0;
-	ptr->index_write = 0;
-	return(ptr);
+	c = buf->buffer[buf->read_index];
+	buf->read_index++;
+	return (c);
 }
 
-int	storage_setup(t_buffer	*b_ptr)
+int	buffer_reset(t_buffer *buf, int fd)
 {
-	b_ptr->storage_count = 0;
-	b_ptr->malloc_size = BUFFER_SIZE;
-	b_ptr->resizable_storage = (char *)malloc(sizeof(char) * b_ptr->malloc_size);
-	if (b_ptr->resizable_storage == NULL)
-		return (0);
+	buf->read_index = 0;
+	buf->write_index = read(fd, buf->buffer, BUFFER_SIZE);
+	return (buf->write_index > 0);
+}
+
+int	buffer_write(int fd, t_buffer *buf)
+{
+	if (buf->read_index >= buf->write_index)
+		if (!buffer_reset(buf, fd))
+			return (0);
 	return (1);
 }
 
-int	realloc_storage(t_buffer *b_ptr)
+char	*get_next_line(int fd)
 {
-	char			*new_storage;
-	unsigned int	i;
-
-	b_ptr->malloc_size *= 2;
-	new_storage = NULL;
-	new_storage = (char *)malloc(sizeof(char) * b_ptr->malloc_size);
-	if (new_storage != NULL)
-	{
-		i = 0;
-		while (i < b_ptr->storage_count)
-		{
-			new_storage[i] = b_ptr->resizable_storage[i];
-			i++;
-		}
-		free(b_ptr->resizable_storage);
-		b_ptr->resizable_storage = new_storage;
-		return (1);
-	}
-	return (0);
-}
-
-int	storechar(t_buffer *b_ptr)
-{
-	if (b_ptr->storage_count == b_ptr->malloc_size)
-	{
-		if (realloc_storage(b_ptr) == 0)
-			return (0);
-	}
-	b_ptr->resizable_storage[b_ptr->storage_count] = b_ptr->buffer[b_ptr->index_read];
-	b_ptr->storage_count++;
-	return (1);
-}
-
-int	buffer_write(int fd, t_buffer *b_ptr)
-{
-	int	bytes_read;
-	
-	bytes_read = read(fd, b_ptr->buffer, BUFFER_SIZE);
-	if (bytes_read == -1)
-		return (0);
-	if (bytes_read == 0)
-		return (2);
-	b_ptr->index_write += bytes_read;
-	return (1);
-}
-
-int	buffer_read(int fd, t_buffer *b_ptr)
-{
-	int	check;
-
-	if (b_ptr->index_read == b_ptr->index_write)
-	{
-		b_ptr->index_read = 0;
-		b_ptr->index_write = 0;
-	}
-	if (b_ptr->index_write == 0)
-	{
-		check = buffer_write(fd, b_ptr);
-		if (check == 0)
-			return (0);
-		if (check == 2)
-			return (2);
-	}
-	while (b_ptr->index_read < b_ptr->index_write)
-	{
-		if (storechar(b_ptr) == 0)
-			return (0);
-		if (b_ptr->buffer[b_ptr->index_read++] == '\n')
-			break;
-	}
-	if (b_ptr->buffer[b_ptr->index_read - 1] != '\n' && b_ptr->index_write == BUFFER_SIZE)
-		buffer_read(fd, b_ptr);
-	return(1);
-}
-
-char	*copy_to_ret(t_buffer *b_ptr)
-{
+	static t_buffer	buffer_arr[256];
+	t_string		*str;
 	char			*ret;
-	unsigned int	i;
 
+	if (fd < 0 || fd >= 256)
+		return (NULL);
 	ret = NULL;
-	ret = (char *)malloc(sizeof(char) * (b_ptr->storage_count + 1));
-	if (ret != NULL)
+	str = string_initialize();
+	while (1)
 	{
-		i = 0;
-		while (i < b_ptr->storage_count)
-		{
-			ret[i] = b_ptr->resizable_storage[i];
-			i++;
-		}
-		ret[i] = '\0';
-		free(b_ptr->resizable_storage);
-		return (ret);
+		if (!buffer_write(fd, buffer_arr + fd))
+			break ;
+		if (!string_store_char(str, buffer_arr + fd))
+			return (NULL);
+		if (str->string[str->fill_count - 1] == '\n')
+			break ;
 	}
-	return (NULL);
-}
-
-char	*get_next_line (int fd)
-{
-	int				check;
-	char			*ret;
-	static t_buffer	*b_ptr;
-
-	if(b_ptr == NULL)
-		b_ptr = buffer_setup();
-	if(b_ptr == NULL)
-	{
-		free(b_ptr);
-		return (NULL);
-	}
-	if(storage_setup(b_ptr) == 0)
-		return (NULL);
-	check = buffer_read(fd, b_ptr);
-	if (check == 0 || check == 2)
-	{
-		free(b_ptr->resizable_storage);
-		free(b_ptr);
-		b_ptr = NULL;
-		return (NULL);
-	}
-	ret = copy_to_ret(b_ptr);
-	if (ret == NULL)
-		return (NULL);
+	string_return(str, &ret);
+	string_delete(str);
 	return (ret);
 }
-
-/*
-	For using get_next_line with multiple fds:
-		- I could make something like a fixed array like: int fd_array[1024];
-		- I want to save the values of index_read and index_write to correspond to their specific file descriptor.
-		- I can make a function that looks for the file descriptor, and if it doesn't find it we create a new entry for that file descriptor.
-	Also clean up your main. You can do it!
-*/
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,30 +6,42 @@
 /*   By: jmaalouf <jmaalouf@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 16:24:09 by jmaalouf          #+#    #+#             */
-/*   Updated: 2022/05/17 17:10:41 by jmaalouf         ###   ########.fr       */
+/*   Updated: 2022/05/21 14:46:07 by jmaalouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 typedef struct s_circular_buffer
 {
-	char			buffer[BUFFER_SIZE];
-	char			*resizable_storage;
-	unsigned int	storage_count;
-	unsigned int	malloc_size;
-	unsigned int	index_write;
-	unsigned int	index_read;
-}					t_buffer;
+	int		read_index;
+	int		write_index;
+	char	buffer[BUFFER_SIZE];
+}			t_buffer;
 
-char	*get_next_line (int fd);
+typedef struct s_resizable_string
+{
+	char			*string;
+	unsigned int	malloc_size;
+	unsigned int	fill_count;
+}					t_string;
+
+char		*get_next_line(int fd);
+void		string_delete(t_string *str);
+void		string_return(t_string *str, char **ret);
+int			string_realloc(t_string *str);
+int			string_store_char(t_string *str, t_buffer *buf);
+t_string	*string_initialize(void);
+char		buffer_read_char(t_buffer *buf);
+int			buffer_reset(t_buffer *buf, int fd);
+int			buffer_write(int fd, t_buffer *buf);
 
 #endif
